c39680a8f500f8737091bded68f28abe74d1ed34
volodg <gorbenko.vova@gmail.com>
2015-06-05 15:39:42 +0300

    initial commits

---
+++ Podfile
@@ -0,0 +1,18 @@
+platform :ios, '8.0'
+use_frameworks!
+
+def import_pods
+
+pod 'iAsync.async'
+pod 'iAsync.utils'
+pod 'iAsync.network'
+
+end
+
+target 'iAsync.social', :exclusive => true do
+  import_pods
+end
+
+target 'iAsync.socialTests', :exclusive => true do
+  import_pods
+end
---
+++ iAsync.social/iAsync.social.h
@@ -0,0 +1,19 @@
+//
+//  iAsync.social.h
+//  iAsync.social
+//
+//  Created by Volodymyr on 05.06.15.
+//  Copyright (c) 2015 EmbeddedSources. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iAsync.social.
+FOUNDATION_EXPORT double iAsync_socialVersionNumber;
+
+//! Project version string for iAsync.social.
+FOUNDATION_EXPORT const unsigned char iAsync_socialVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iAsync_social/PublicHeader.h>
+
+
